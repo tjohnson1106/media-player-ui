15d2cee46182b3981a8bb6ea95da635f7d17cb1b
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2018-03-31 15:35:29 -0400

    created scroll and scroll offset functionality

--- media-player/src/MusicUI.js
+++ media-player/src/MusicUI.js
@@ -16,7 +16,13 @@ const SCREEN_HEIGHT = Dimensions.get("window").height;
 const SCREEN_WIDTH = Dimensions.get("window").width;
 
 class MusicUI extends Component {
+  state = {
+    isScrollEnabled: false
+  };
+
   componentWillMount() {
+    this.scrollOffset = 0;
+
     this.animation = new Animated.ValueXY({
       x: 0,
       y: SCREEN_HEIGHT - 90
@@ -102,80 +108,140 @@ class MusicUI extends Component {
             }
           ]}
         >
-          <Animated.View
-            style={{
-              height: animatedHeaderHeight,
-              borderTopWidth: 1,
-              borderTopColor: "#ebe5e5",
-              flexDirection: "row",
-              alignItems: "center"
+          <ScrollView
+            scrollEnabled={this.state.isScrollEnabled}
+            scrollEventThrottle={16}
+            onScroll={event => {
+              this.scrollOffset = event.nativeEvent.contentOffset.y;
             }}
           >
-            <View
+            <Animated.View
               style={{
-                flex: 4,
+                height: animatedHeaderHeight,
+                borderTopWidth: 1,
+                borderTopColor: "#ebe5e5",
                 flexDirection: "row",
                 alignItems: "center"
               }}
             >
+              <View
+                style={{
+                  flex: 4,
+                  flexDirection: "row",
+                  alignItems: "center"
+                }}
+              >
+                <Animated.View
+                  style={{
+                    height: animatedImageHeight,
+                    width: animatedImageHeight,
+                    marginLeft: animatedImageMarginLeft
+                  }}
+                >
+                  <Image
+                    style={{
+                      flex: 1,
+                      width: null,
+                      height: null
+                    }}
+                    source={require("../assets/media_girl.jpeg")}
+                  />
+                </Animated.View>
+                <Animated.Text
+                  style={{
+                    opacity: animatedSongTitleOpacity,
+                    fontSize: 18,
+                    paddingLeft: 10
+                  }}
+                >
+                  Barracuda
+                </Animated.Text>
+              </View>
               <Animated.View
                 style={{
-                  height: animatedImageHeight,
-                  width: animatedImageHeight,
-                  marginLeft: animatedImageMarginLeft
-                }}
-              >
-                <Image
-                  style={{
-                    flex: 1,
-                    width: null,
-                    height: null
-                  }}
-                  source={require("../assets/media_girl.jpeg")}
-                />
-              </Animated.View>
-              <Animated.Text
-                style={{
                   opacity: animatedSongTitleOpacity,
-                  fontSize: 18,
-                  paddingLeft: 10
+                  flex: 1,
+                  flexDirection: "row",
+                  justifyContent: "space-around"
                 }}
               >
-                UNTITLED(Live)
-              </Animated.Text>
-            </View>
+                <Ionicons name="md-pause" size={32} />
+                <Ionicons name="md-play" size={32} />
+              </Animated.View>
+            </Animated.View>
+
             <Animated.View
               style={{
-                opacity: animatedSongTitleOpacity,
-                flex: 1,
-                flexDirection: "row",
-                justifyContent: "space-around"
+                height: animatedHeaderHeight,
+                opacity: animatedSongDetailsOpacity
               }}
             >
-              <Ionicons name="md-pause" size={32} />
-              <Ionicons name="md-play" size={32} />
-            </Animated.View>
-          </Animated.View>
+              <View
+                style={{
+                  flex: 1,
+                  alignItems: "center",
+                  justifyContent: "flex-end"
+                }}
+              >
+                <Text
+                  style={{
+                    fontWeight: "bold",
+                    fontSize: 22
+                  }}
+                >
+                  Barracuda
+                </Text>
+                <Text
+                  style={{
+                    fontSize: 18,
+                    color: "#fa95ed"
+                  }}
+                >
+                  Heart - Little Queen
+                </Text>
 
-          <Animated.View
-            style={{
-              height: animatedHeaderHeight,
-              opacity: animatedSongDetailsOpacity
-            }}
-          >
-            <View
-              style={{
-                flex: 2,
-                flexDirection: "row",
-                alignItems: "center",
-                justifyContent: "space-around"
-              }}
-            >
-              <Ionicons name="md-rewind" size={40} />
-              <Ionicons name="md-pause" size={50} />
-              <Ionicons name="md-fastforward" size={40} />
-            </View>
-          </Animated.View>
+                <View
+                  style={{
+                    height: 40,
+                    width: SCREEN_WIDTH,
+                    alignItems: "center"
+                  }}
+                >
+                  <Slider
+                    style={{ width: 300 }}
+                    step={1}
+                    minimumValue={18}
+                    maximumValue={71}
+                    value={18}
+                  />
+                </View>
+              </View>
+
+              <View
+                style={{
+                  flex: 2,
+                  flexDirection: "row",
+                  alignItems: "center",
+                  justifyContent: "space-around"
+                }}
+              >
+                <Ionicons name="md-rewind" size={40} />
+                <Ionicons name="md-pause" size={50} />
+                <Ionicons name="md-fastforward" size={40} />
+              </View>
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  paddingHorizontal: 20,
+                  paddingBottom: 20
+                }}
+              >
+                <Ionicons name="md-add" size={32} style={{ color: "#fa95ed" }} />
+                <Ionicons name="md-more" size={32} style={{ color: "#fa95ed" }} />
+              </View>
+            </Animated.View>
+          </ScrollView>
         </Animated.View>
       </Animated.View>
     );
